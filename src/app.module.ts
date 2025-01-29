51bd7cb7377271fa85b67fd61ba1d3f051bf8065
kyungeun <kyungeun9718@daum.net>
2025-01-29 18:49:24 +0900

    acoc초기 세팅

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ormConfig } from './orm.config';
+import { ProductModule } from './domain/product/product.module';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRootAsync({ useFactory: ormConfig }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
